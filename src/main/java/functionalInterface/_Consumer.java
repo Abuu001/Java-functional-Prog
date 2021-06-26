3516ba224c6977b74d6003ebaafcaac324b27ab0
Abuu001 <abulugz@gmail.com>
2021-06-26 23:40:46 +0300

    used for func tht returns void

--- src/main/java/functionalInterface/_Consumer.java
+++ src/main/java/functionalInterface/_Consumer.java
@@ -7,30 +7,31 @@ public class _Consumer {
     public static void main(String[] args) {
 
         //normal func
-        Customer faith =new Customer("Faith","07251525156");
+        Customer faith = new Customer("Faith", "07251525156");
         greetCustomer(faith);
 
         //consumer func
-        Customer maria =new Customer("Maria","07238121834");
+        Customer maria = new Customer("Maria", "07238121834");
         greetCustomerConsumer.accept(maria);
     }
 
     //consumer func
-    static Consumer<Customer> greetCustomerConsumer= customer ->
-            System.out.println("Hello "+customer.customerName +", thanks for registering phone No :" + customer.customerPhoneNumber);
+    //consumers r used for void functions
+    static Consumer<Customer> greetCustomerConsumer = customer ->
+            System.out.println("Hello " + customer.customerName + ", thanks for registering phone No :" + customer.customerPhoneNumber);
 
     //normal func
-    static  void  greetCustomer(Customer customer){
-        System.out.println("Hello "+customer.customerName +", thanks for registering phone No :" + customer.customerPhoneNumber);
+    static void greetCustomer(Customer customer) {
+        System.out.println("Hello " + customer.customerName + ", thanks for registering phone No :" + customer.customerPhoneNumber);
     }
 
-    static class Customer{
+    static class Customer {
         public final String customerName;
-        public final String  customerPhoneNumber;
+        public final String customerPhoneNumber;
 
-        Customer(String customerName,String customerPhoneNumber){
-            this.customerName=customerName;
-            this.customerPhoneNumber=customerPhoneNumber;
+        Customer(String customerName, String customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
         }
     }
 }
