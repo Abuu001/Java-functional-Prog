753a3392e10e7a6485539bdafc9369578fc896b5
Abuu001 <abulugz@gmail.com>
2021-06-27 12:12:39 +0300

    method refer3

--- src/main/java/MethodReference/MethodReferenceComparator.java
+++ src/main/java/MethodReference/MethodReferenceComparator.java
@@ -1,2 +1,20 @@
-package MethodReference;public class MethodReferenceComparator {
+package MethodReference;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MethodReferenceComparator {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,2,3,4);
+            //normal
+        numbers.forEach(p-> System.out.println(p));
+
+        System.out.println("=========Lambda expression============");
+        numbers.forEach(System.out::println);
+
+        List<String> names = Arrays.asList("Abu","sid","kim");
+        names.forEach(System.out::println);
+    }
+
 }
